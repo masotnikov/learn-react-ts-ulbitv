2c8b13c576c33f7782160314ef00eecfabdf78fb
masotnikov <motrunich2011@gmail.com>
2024-01-04 00:35:10 +0500

    created component Card

---
+++ src/components/Card.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface CardProps {
+    width?: string,
+    height?: string,
+    children: React.ReactNode
+}
+
+const Card = ({width, height, children} : CardProps) => {
+    return (
+        <div style={{width, height, border: '1px solid grey'}}>
+            {children}
+        </div>
+    )
+}
+
+export default Card;
