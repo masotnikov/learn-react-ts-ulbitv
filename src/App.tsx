5e7499d0340f48be95cecb282d665ca1fb9bb12c
masotnikov <motrunich2011@gmail.com>
2024-01-04 00:35:30 +0500

    created button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-
+import Card from "./components/Card";
 
 const App = () => {
     return (
         <div>
-            
+            <Card height="200px" width="200px">
+                <button>Кнопка</button>
+            </Card>
         </div>
     )
 }
